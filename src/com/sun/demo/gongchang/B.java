54d835543e793bf25fe1cac60f40010ed756dd54
sunhang <sh003762>
2019-03-01 15:18:19 +0800

    commit

---
+++ src/com/sun/demo/gongchang/B.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C), 2015-2019, 壹永科技有限公司
+ * FileName: B
+ * Author:   sun
+ * Date:     2019/3/1 15:18
+ * History:
+ * <author>          <time>                <version>
+ *   sun         2019/3/1 15:18           v1.0.0
+ */
+package com.sun.demo.gongchang;
+
+/**
+ * 一句话功能简述
+ *
+ * @author sun
+ * @create 2019/3/1
+ * @since v1.0.0
+ */
+public class B {
+}
