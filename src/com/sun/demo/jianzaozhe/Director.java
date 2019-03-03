1dea2f3fa16482c155ba6d20f6e9c244b468d6ee
bengdabengda <sh18513254171>
2019-03-03 22:09:18 +0800

    建造者模式

---
+++ src/com/sun/demo/jianzaozhe/Director.java
@@ -0,0 +1,29 @@
+package com.sun.demo.jianzaozhe;
+
+/**
+ * 指挥者对象，控制对象的创建过程，顺序等等
+ */
+public class Director {
+    private Build build;
+
+    public Director(Build build){
+        this.build = build;
+    }
+
+    public Person createPerson(){
+        if(build.neadHead()){
+            build.buildHead();
+        }
+        build.buildBody();
+        build.buildHand();
+        return build.buildPerson();
+    }
+
+    public Build getBuild() {
+        return build;
+    }
+
+    public void setBuild(Build build) {
+        this.build = build;
+    }
+}
