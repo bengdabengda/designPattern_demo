7e71f3e538594c974004a2a4f804f94d46a86c30
bengdabengda <sh18513254171>
2019-03-03 14:49:40 +0800

    单例模式静态内部类实现

---
+++ src/com/sun/demo/danli/Singleton.java
@@ -0,0 +1,17 @@
+package com.sun.demo.danli;
+
+/**
+ * 使用静态内部类，实现懒汉模式中的懒加载，同时实现饿汉模式中的线程安全
+ */
+public class Singleton {
+    private Singleton() {
+    }
+
+    private static class HolderClass {
+        private final static Singleton instance = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return HolderClass.instance;
+    }
+}
